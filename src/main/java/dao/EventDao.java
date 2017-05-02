85914e38ee69e01d320a2d387b83f5bd90e629e3
siematypie <mkkc12@gmail.com>
2017-05-02 17:09:59 +0200

    Change EventDao to interface, EventDaoHardcoded reurns list of events

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -1,7 +1,9 @@
 package dao;
 
-/**
- * Created by rafalstepien on 28/04/2017.
- */
-public class EventDao {
+import models.Event;
+import java.util.List;
+
+public interface EventDao {
+    public Event getEventById(Integer id);
+    public List<Event> getAllEvents();
 }
