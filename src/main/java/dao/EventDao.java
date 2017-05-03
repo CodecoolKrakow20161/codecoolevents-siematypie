82df3f6a65ca4ddfc4d6be61e1a2d8ccfc102343
siematypie <mkkc12@gmail.com>
2017-05-03 21:33:39 +0200

    Add addOrUpdateEvent method to eventDao

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -4,6 +4,7 @@ import models.Event;
 import java.util.List;
 
 public interface EventDao {
-    public Event getEventById(Integer id);
-    public List<Event> getAllEvents();
+     void addOrUpdateEvent(Event event);
+     Event getEventById(Integer id);
+     List<Event> getAllEvents();
 }
