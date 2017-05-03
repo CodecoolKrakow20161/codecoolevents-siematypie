5e51cc52c36d841e2adfed073fca713fa9631196
siematypie <mkkc12@gmail.com>
2017-05-03 23:43:48 +0200

    Create table categories in db

--- src/main/resources/sqls/createTables.sql
+++ src/main/resources/sqls/createTables.sql
@@ -5,3 +5,8 @@ CREATE TABLE IF NOT EXISTS events(
                         description TEXT,
                         category TEXT
                         );
+
+CREATE TABLE IF NOT EXISTS categories(
+                     	id SERIAL PRIMARY KEY,
+                        name TEXT UNIQUE
+                        );
