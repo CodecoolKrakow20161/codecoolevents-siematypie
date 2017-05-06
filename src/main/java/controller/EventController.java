f3b995eb2f97fc6239fda44a696a054584e58e7f
siematypie <mkkc12@gmail.com>
2017-05-06 16:47:56 +0200

    Events are now converted to json via EventController

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -3,17 +3,27 @@ package controller;
 import dao.Dao;
 import dao.EventDaoPostgres;
 import models.Event;
+import utils.JsonTransformer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EventController {
     private static Dao<Event> dao = new EventDaoPostgres();
+    private static JsonTransformer jsonTransformer = new JsonTransformer();
 
      static Map<String,Object> getEvents() {
         //Get events from database by Dao
          HashMap<String, Object> eventMap = new HashMap<>();
          eventMap.put("eventContainer", dao.getAll());
          return eventMap;
+    }
+
+    public static String getEventJson(int eventId){
+        Event event = dao.getById(eventId);
+        if (event == null) {
+            throw new IllegalArgumentException("Requested event not found in the database");
+        }
+        return jsonTransformer.render(event);
     }
 }
