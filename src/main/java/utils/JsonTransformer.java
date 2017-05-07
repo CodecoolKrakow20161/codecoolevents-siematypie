4a731f4db3e6e8a67779ff3bec833bcc4509e7e2
siematypie <mkkc12@gmail.com>
2017-05-07 16:40:29 +0200

    Add method for parsing json to list of objects

--- src/main/java/utils/JsonTransformer.java
+++ src/main/java/utils/JsonTransformer.java
@@ -2,10 +2,14 @@ package utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import spark.ResponseTransformer;
 
+import java.util.List;
+
 public class JsonTransformer implements ResponseTransformer {
-    public Gson gson = new GsonBuilder()
+    private Gson gson = new GsonBuilder()
             .setDateFormat("dd-MM-yyyy").create();
 
     @Override
@@ -13,4 +17,11 @@ public class JsonTransformer implements ResponseTransformer {
         return gson.toJson(model);
     }
 
+    public JsonObject getJsonObject(String line){
+        return gson.fromJson(line, JsonObject.class);
+    }
+
+    public <T> List<T> parseToList(String obj, Class<T> classe){
+        return  gson.fromJson(obj, new TypeToken<List<T>>(){}.getType());
+    }
 }
